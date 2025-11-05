054497697c4a0466a27bb9bc17a5596a67169a3c
GG-00000 <ggutierrez48@cps.edu>
2025-11-05 20:36:54 +0000

    Bellringer

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -45,3 +45,45 @@
 # Length and Count:
 # a. Calculate the number of characters (including spaces and punctuation) in the word/phrase: "Supercalifragilisticexpialidocious".
 # b. Count the number of times the letter 'i' appears in the same word/phrase.
+magic = "abracadabra"
+print(magic[4])
+print(magic[-2])
+print(magic.index('c'))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+hij = alphabet[7:10]
+print(alphabet[7:10])
+print(alphabet[:13:2])
+print(alphabet[::-1])
+speech =  "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
+print(speech.find('John F. Kennedy'))
+speech_name = speech[83:]
+print(speech_name)
+
+info = "Python is fun. Fun is good. Good is subjective."
+print(info.rfind('subjective'))
+subjective_wrd = info[36:]
+print(subjective_wrd)
+every_third = info.split()[::3]
+reversed_words = ' '.join(info.split()[::-1])
+
+text = "MAY THE FORCE BE WITH YOU."
+lower_text = text.lower()
+
+motto = ["Make", "haste", "slowly."]
+joined_motto = ' '.join(motto)
+split_on_a = joined_motto.split('a')
+
+sentence = "Life is what happens when you are busy making other plans."
+replaced_busy = sentence.replace("busy", "distracted")
+replaced_plans = sentence.replace("plans", "mistakes")
+
+word = "Iteration"
+repeated_word = word * 7
+
+quote2 = "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
+has_moonlight = "moonlight" in quote2
+
+phrase = "Supercalifragilisticexpialidocious"
+num_chars = len(phrase)
+count_i = phrase.count('i')
